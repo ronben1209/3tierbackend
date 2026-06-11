5023a66b1ad251acb6989248403bca4a52bd0824
ronben1209 <ronben1209@gmail.com>
2026-06-11 15:29:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     // UPDATE ALL THESE THINGS
     // HOST ==> MYSQL Endpoints
-    host: "",
-    user: "",
-    password: "",
-    database: ""
+    host: "threetierdarabase.cp8cs448ijwp.ap-south-1.rds.amazonaws.com",
+    user: "admin",
+    password: "Jesuscares1209$",
+   // database: "taskdb"
 });
 
 connection.connect();
